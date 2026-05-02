536238bc8e7e64db7629f8b815144b829bbc11ac
Your Name <your@email.com>
2026-05-02 23:55:25 +0530

    fixed run block

--- app.py
+++ app.py
@@ -405,5 +405,8 @@ def remove_admin(email):
     return redirect("/users")
 
 # ---------------- RUN ----------------
+    import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
